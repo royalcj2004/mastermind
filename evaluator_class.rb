d5561e00664218e80c37aebff237c529e505b57c
unknown <Chijioke Elisha-Wigwe>
2015-09-20 20:55:45 +0100

    The evaluator_class tested and tried.

--- evaluator_class.rb
+++ evaluator_class.rb
@@ -1,53 +1,52 @@
+require 'pry'
 class Evaluator
 	def self.compare(secret_code, guess)
-	    secret_hash = Hash.new(0)
-	    secret_code.inject(secret_hash) do |m,v| 
-	        m[v] += 1
-	        m
+		secret_hash = get_hash_from_array_with_number_of_occurences(secret_code)
+		guess_hash = get_hash_from_array_with_number_of_occurences(guess)
+		hash_of_total_matches = get_hash_of_total_matches_between(secret_hash,guess_hash)
+	    number_of_total_matches = hash_of_total_matches.values.reduce(:+)
+	    number_of_total_matches ||= 0
+	   	number_of_exact_matches = get_number_of_exact_matches(secret_code, guess)
+	   	number_of_partial_matches = number_of_total_matches - number_of_exact_matches
+	   	hash_of_exact_and_partial = {}
+	   	hash_of_exact_and_partial[:exact] = number_of_exact_matches
+	   	hash_of_exact_and_partial[:partial] = number_of_partial_matches
+	   	hash_of_exact_and_partial
+	end
+
+	private
+
+	def self.get_hash_from_array_with_number_of_occurences(code_array)
+		code_hash = Hash.new(0)
+		code_array.inject(code_hash) do |m,v|
+			m[v] += 1
+			m
+		end
+		code_hash
+	end
+
+	def self.get_hash_of_total_matches_between(comp_hash, user_hash)
+		hash_of_total = {}
+		user_hash.map do |key,value|
+	        if  comp_hash.has_key?(key)
+                hash_of_total[key] = user_hash[key] if value <= comp_hash[key]
+                hash_of_total[key] = comp_hash[key] if value > comp_hash[key]
+            end
 	    end
-	    guess_hash = Hash.new(0)
-	    guess.inject(guess_hash) do |m,v|
-	        m[v] += 1
-	        m
-	    end
-	    matched_hash = {}
-	    guess_hash.map do |key,value|
-	        if  secret_hash.has_key?(key)
-                matched_hash[key] = guess_hash[key] if value <= secret_hash[key]
-                matched_hash[key] = secret_hash[key] if value > secret_hash[key]
-            else
-  	        end
-	    end
-	    number_of_correct_guesses = matched_hash.values.reduce(:+)
-	   	exact = 0
-		unexact = 0
-		partial = 0
-		exact_array = []
-		partial_array_comp = []
-		partial_array_user = []
-		result_hash = Hash.new
-		new_hash = Hash[secret_code.zip(guess)]
-		combined = secret_code.zip(guess)
+	    hash_of_total
+	end
+
+	def self.get_number_of_exact_matches(comp, guess)
+		exact = 0
+		combined = comp.zip(guess)
 		combined.each do |one_combination|
 				if one_combination[0] == one_combination[1]
 					exact += 1
-					exact_array << one_combination
-				else
-					unexact += 1
-					partial_array_comp << one_combination[0]
-					partial_array_user << one_combination[1]
 				end
 		end
-		partial_array_user.select do |i| 
-		partial += 1 if partial_array_comp.include? (i)
-		end
-		if number_of_correct_guesses == nil
-		    number_of_correct_guesses = 0
-		end
-		result_hash[:exact] = exact
-		result_hash[:unexact] = unexact
-		result_hash[:partial] = number_of_correct_guesses-exact
-		result_hash[:number_of_correct_guesses] = number_of_correct_guesses
-		result_hash
+		exact
 	end
+
 end
+
+Evaluator.compare(["r","r","r","r"],["r","r","r","r"])
